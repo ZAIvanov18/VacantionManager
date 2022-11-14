d2c95308f97123e20e30fe343fc14adb9ad91a18
ZAIvanov18 <ZAIvanov18@codingburgas.bg>
2022-11-14 11:49:24 +0200

    Initial commit

---
+++ src/main/java/com/example/vacantionmanager/VacantionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacantionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacantionManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacantionManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vacantionmanager/VacantionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacantionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacantionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
